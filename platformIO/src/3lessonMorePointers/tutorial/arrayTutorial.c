09cd1ee76cf3f4bea6da8160fd3ca1ed1728991d
Jakub Kuka <jakubkuka07@gmail.com>
2024-06-06 09:46:37 +0200

    String tutorial

--- platformIO/src/3lessonMorePointers/tutorial/arrayTutorial.c
+++ platformIO/src/3lessonMorePointers/tutorial/arrayTutorial.c
@@ -1,8 +1,12 @@
+#include <stdio.h>
+
 int main(void){
 
-    int grade1 = 91;
-    int grade2 = 82;
-    int grade3 = 73;
-     
+    int grade[] = {91,82,73,64,55};
+
+    for (int i = 0; i < 5; i++){
+        printf("grade[%d] = %d\n", i, grade[i]);
+    }
+
     return 0;
 }
