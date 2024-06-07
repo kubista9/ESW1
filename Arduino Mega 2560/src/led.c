6be6990044a414f61d64fab1ede55aaa2326dbf3
Jakub Kuka <jakubkuka07@gmail.com>
2024-06-07 12:07:05 +0200

    Exercise 6 button

--- Arduino Mega 2560/src/led.c
+++ Arduino Mega 2560/src/led.c
@@ -3,18 +3,20 @@
 
 #define DDR_LED DDRB
 #define PORT_LED PORTB
+#define LED_PIN PB7
 
-void led_init(){
-    DDR_LED|=(1<<PB7);
+void led_init() {
+    DDR_LED |= (1 << LED_PIN);  // Set PB7 as output
 }
 
+void led_on() {
+    PORT_LED |= (1 << LED_PIN);  // Turn on LED
+}
 
-void led_on(){
-    PORT_LED|=(1<<PB7);
+void led_off() {
+    PORT_LED &= ~(1 << LED_PIN);  // Turn off LED
 }
-void led_off(){
-    PORT_LED&=~(1<<PB7);
-}
-void led_toggle(){
-    PORT_LED^=(1<<PB7);
+
+void led_toggle() {
+    PORT_LED ^= (1 << LED_PIN);  // Toggle LED state
 }
--- Arduino Mega 2560/src/main.c
+++ Arduino Mega 2560/src/main.c
@@ -2,14 +2,21 @@
 #include <util/delay.h>
 #include "led.h"
 
+void button_init() {
+    DDRB &= ~(1 << PB0);  // Set PB0 as input
+    PORTB |= (1 << PB0);  // Enable pull-up resistor on PB0
+}
+
 int main(void) {
     led_init();
+    button_init();
     
     while (1) {
-        led_on();
-        _delay_ms(500);
-        led_off();
-        _delay_ms(500);
+        if (!(PINB & (1 << PB0))) {
+            led_on();  // Turn on LED when button is pressed
+        } else {
+            led_off();  // Turn off LED when button is not pressed
+        }
     }
 
     return 0;
