dd80050bd1b551acb8353ad90dd00d49cb7440c9
Jakub Kuka <jakubkuka07@gmail.com>
2024-06-10 17:37:58 +0200

    Parts of exercise 3.2

--- platformIO/src/5lessonOpaquePattern/exercises/3.1-Car/car.c
+++ platformIO/src/5lessonOpaquePattern/exercises/3.1-Car/car.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+
 struct car_s{
     char  brand[30];
     char  model[30]; 
--- platformIO/src/5lessonOpaquePattern/exercises/3.1-Car/car.h
+++ platformIO/src/5lessonOpaquePattern/exercises/3.1-Car/car.h
@@ -1,23 +1,13 @@
 typedef struct car_s * car_t;
 
 car_t car_create(const char * brand,const char * model, int horse_power, double fuel_capacity, double current_fuel, double fuel_efficientcy);
-
 char * car_get_brand(car_t self);
-
 char * car_get_model(car_t self);
-
 int car_get_horsepower(car_t self);
-
 double car_get_fuel_capacity(car_t self);
-
 double car_get_curret_fuel(car_t self);
-
 double car_get_fuel_Efficientcy(car_t self);
-
 double car_distance2empty(car_t self);
-
 void car_print_info(car_t self);
-
 void car_boost_horsepower(car_t self, int increased_hp);
-
 void car_destroy(car_t self);
