ac679aeeec31cac1e2bc3eb5412bd1b7ce17b756
Jakub Kuka <jakubkuka07@gmail.com>
2024-06-10 15:03:53 +0200

    Small update on exercise

--- platformIO/src/4lessonOopUnitTests/exercises/Ex4.1-squareOOP/square.c
+++ platformIO/src/4lessonOopUnitTests/exercises/Ex4.1-squareOOP/square.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include "square.h"
 
-// -> is used for accessing members ( variables, methods )of a structure or a class through pointer
+// -> is used for accessing members ( variables, methods )of a structure 
+// or a class through pointer
 void square_create(square_t *square, int length){
     square -> length = length;
     square -> area = length * length;
     square -> perimeter = length * 4;
 }
 
-// By declaring the pointer parameter as const, you are indicating that the function will not modify the square_t
+// By declaring the pointer parameter as const, you are indicating 
+// that the function will not modify the square_t
 
 int square_get_sl(const square_t *square){
     return square -> length;
--- platformIO/src/4lessonOopUnitTests/exercises/Ex4.1-squareOOP/square.h
+++ platformIO/src/4lessonOopUnitTests/exercises/Ex4.1-squareOOP/square.h
@@ -1,7 +1,9 @@
-// checks if SQUARE_H has not been defined. If it hasn't, the preprocessor includes the code that follows up to the next #endif directive.
+// checks if SQUARE_H has not been defined. If it hasn't, the preprocessor 
+// includes the code that follows up to the next #endif directive.
 #ifndef SQUARE_H
 
-// defines the macro SQUARE_H. This is typically done immediately after the #ifndef check to ensure that the macro is defined for subsequent checks in the same compilation unit
+// defines the macro SQUARE_H. This is typically done immediately after the #ifndef 
+//check to ensure that the macro is defined for subsequent checks in the same compilation unit
 #define SQUARE_H
 
 typedef struct {
@@ -11,7 +13,8 @@ typedef struct {
 } square_t;
 
 
-// '*' - dereference a pointer,  In C, pointers are variables that store the memory address of another variable
+// '*' - dereference a pointer,  In C, pointers are variables that store the 
+// memory address of another variable
 void square_create(square_t *square, int length);
 int square_get_sl(const square_t *square);
 int square_get_area(const square_t *square);
