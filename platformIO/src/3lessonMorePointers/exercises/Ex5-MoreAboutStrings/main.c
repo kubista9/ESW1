d5eeef19c68a811ae6656258374661c6a9dc605a
Jakub Kuka <jakubkuka07@gmail.com>
2024-06-10 14:13:57 +0200

    Ex 5 more about t strins

--- platformIO/src/3lessonMorePointers/exercises/Ex5-MoreAboutStrings/main.c
+++ platformIO/src/3lessonMorePointers/exercises/Ex5-MoreAboutStrings/main.c
@@ -7,12 +7,7 @@ int main() {
     char line[MAX_LENGTH];
     char upper[MAX_LENGTH];
     int line_length;
-    
-    /* Use a while loop to read input lines as long as there are any.
-    For each input line, print the length of the line as well as
-    the upper case version of the line
-    NOTE You are NOT allowed to use the standard toupper(...)
-    function in string.h */
+
     printf("Enter a line: ");
     while (read_line(line) > 0) {
         line_length = my_length(line);
--- platformIO/src/3lessonMorePointers/exercises/Ex5-MoreAboutStrings/my_string_func.c
+++ platformIO/src/3lessonMorePointers/exercises/Ex5-MoreAboutStrings/my_string_func.c
@@ -1,27 +1,15 @@
 #include <stdio.h>
 #include "my_string_func.h"
 
-/* read_line: read a line into s, return length */
-
 int my_length(char* text){
-
-    /* Implement this function so it returns the number of characters it is. */
     int length = 0;
     while(text[length] != '\0'){
         length++;
     }
     return length;
-
 }
 
 void my_to_upper(char *str_in, char *str_out) {
-
-    /* You must implement your own version of toupper using the two strings
-    str_in and str_out.
-    Loop over the str_in (as long as it "contains" something).
-    Look at the "current" character and check if its a lowercase letter.
-    If it is, convert it to the upper case version and add that to the
-    str_out character array */
     int i = 0;
     while(str_in[i] != '\0') {
         if(str_in[i] >= 'a' && str_in[i] <= 'z') {
@@ -32,8 +20,6 @@ void my_to_upper(char *str_in, char *str_out) {
         i++;
     }
     str_out[i] = '\0'; // Don't forget to null-terminate the string!
-
-
 }
 
 int read_line(char s[]) {
